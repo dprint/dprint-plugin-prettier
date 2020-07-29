c4861136bd2c5aa62cf0a85005c20828427ee872
David Sherret <dsherret@gmail.com>
2020-07-29 13:32:18 -0400

    Output checksums on CI.

--- scripts/createPluginFile.js
+++ scripts/createPluginFile.js
@@ -20,9 +20,11 @@ function getPlatformObject(zipFileName) {
     const fileBytes = fs.readFileSync(zipFileName);
     const hash = crypto.createHash("sha256");
     hash.update(fileBytes);
+    const checksum = hash.digest("hex");
+    console.log(zipFileName + ": " + checksum);
     return {
         "reference": `https://github.com/dprint/dprint-plugin-prettier/releases/download/${version}/${zipFileName}`,
-        "checksum": hash.digest("hex"),
+        "checksum": checksum,
     };
 }
 
