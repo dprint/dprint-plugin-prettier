9395b0b843028e6741d2ae542f36ff2749324b4b
David Sherret <dsherret@gmail.com>
2022-02-01 11:33:19 -0500

    chore(release): 0.5.1

--- src/messageProcessor.ts
+++ src/messageProcessor.ts
@@ -181,7 +181,7 @@ function sendErrorResponse(message: string) {
 function getPluginInfo() {
   return {
     name: "dprint-plugin-prettier",
-    version: "0.5.0",
+    version: "0.5.1",
     configKey: "prettier",
     fileExtensions: getExtensions(),
     helpUrl: "https://dprint.dev/plugins/prettier",
