5386066abda4763511141371391fbe5d22518f54
David Sherret <dsherret@gmail.com>
2022-03-20 20:27:22 -0400

    Fix startup.test.ts

--- js/startup.test.ts
+++ js/startup.test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.130.0/testing/asserts.ts";
-import "./bundle.js";
+import "./startup.js";
 
 Deno.test("should format typescript", () => {
   assertEquals((globalThis as any).dprint.formatText("test.ts", "const t   = {   }", 1, {}), "const t = {};\n");
