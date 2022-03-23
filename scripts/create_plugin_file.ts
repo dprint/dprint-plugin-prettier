175f5ca0c2479b4faa46b9d40dc93e7b5a8fe38f
David Sherret <dsherret@gmail.com>
2022-03-23 19:30:30 -0400

    Use automation repo script for creating plugin file.

--- scripts/create_plugin_file.ts
+++ scripts/create_plugin_file.ts
@@ -1,37 +1,16 @@
 import * as path from "https://deno.land/std@0.130.0/path/mod.ts";
-import { extractCargoVersion, processPlugin } from "https://raw.githubusercontent.com/dprint/automation/0.1.0/mod.ts";
+import { extractCargoVersion, processPlugin } from "https://raw.githubusercontent.com/dprint/automation/0.2.0/mod.ts";
 
 const currentDirPath = path.dirname(path.fromFileUrl(import.meta.url));
 const cargoFilePath = path.join(currentDirPath, "../", "Cargo.toml");
-const pluginName = "dprint-plugin-prettier";
 
-const builder = new processPlugin.PluginFileBuilder({
-  name: "dprint-plugin-prettier",
+await processPlugin.createDprintOrgProcessPlugin({
+  pluginName: "dprint-plugin-prettier",
   version: await extractCargoVersion(cargoFilePath),
+  platforms: [
+    "darwin-x86_64",
+    "linux-x86_64",
+    "windows-x86_64",
+  ],
+  isTest: Deno.args.some(a => a == "--test"),
 });
-
-if (Deno.args.some(a => a == "--test")) {
-  const platform = processPlugin.getCurrentPlatform();
-  const zipFileName = processPlugin.getStandardZipFileName(builder.pluginName, platform);
-  await builder.addPlatform({
-    platform,
-    zipFilePath: zipFileName,
-    zipUrl: zipFileName,
-  });
-} else {
-  await addPlatform("darwin-x86_64");
-  await addPlatform("linux-x86_64");
-  await addPlatform("windows-x86_64");
-}
-
-await builder.writeToPath("plugin.exe-plugin");
-
-async function addPlatform(platform: processPlugin.Platform) {
-  const zipFileName = processPlugin.getStandardZipFileName(builder.pluginName, platform);
-  const zipUrl = `https://github.com/dprint/${pluginName}/releases/download/${builder.version}/${zipFileName}`;
-  await builder.addPlatform({
-    platform,
-    zipFilePath: zipFileName,
-    zipUrl,
-  });
-}
