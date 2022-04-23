2494ae295a5960c9b0e6a749a971562cfd15fa1a
David Sherret <dsherret@gmail.com>
2022-04-23 19:51:41 -0400

    0.6.3

--- scripts/create_plugin_file.ts
+++ scripts/create_plugin_file.ts
@@ -1,5 +1,5 @@
 import * as path from "https://deno.land/std@0.130.0/path/mod.ts";
-import { extractCargoVersion, processPlugin } from "https://raw.githubusercontent.com/dprint/automation/0.2.0/mod.ts";
+import { extractCargoVersion, processPlugin } from "https://raw.githubusercontent.com/dprint/automation/0.3.0/mod.ts";
 
 const currentDirPath = path.dirname(path.fromFileUrl(import.meta.url));
 const cargoFilePath = path.join(currentDirPath, "../", "Cargo.toml");
