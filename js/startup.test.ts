28104b2564dfd1bebf4fe7811848208cfd512d81
David Sherret <dsherret@gmail.com>
2022-05-06 14:34:51 -0400

    fix: make formatting svelte files work again
    Closes #18

--- js/startup.test.ts
+++ js/startup.test.ts
@@ -1,9 +1,11 @@
 import { assertEquals } from "https://deno.land/std@0.130.0/testing/asserts.ts";
 import "./startup.js";
 
+const { formatText } = (globalThis as any).dprint;
+
 Deno.test("should format typescript", () => {
   assertEquals(
-    (globalThis as any).dprint.formatText(
+    formatText(
       {
         filePath: "test.ts",
         fileText: "const t   = {   }",
@@ -12,5 +14,40 @@ Deno.test("should format typescript", () => {
       {},
     ),
     "const t = {};\n",
+  );
+});
+
+Deno.test("should format svelte", () => {
+  assertEquals(
+    formatText(
+      {
+        filePath: "app.svelte",
+        fileText: `<script>
+  let files;
+</script>
+
+<input type="file" bind:files>
+
+{#if files && files[0]}
+    <p>
+    {files[0].name}
+      </p>
+{/if}`,
+      },
+      1,
+      {},
+    ),
+    `<script>
+  let files;
+</script>
+
+<input type="file" bind:files />
+
+{#if files && files[0]}
+  <p>
+    {files[0].name}
+  </p>
+{/if}
+`,
   );
 });
