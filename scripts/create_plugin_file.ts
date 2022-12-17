5bc3ef537b6f7857420f114f2a669a8966d6c487
David Sherret <dsherret@gmail.com>
2022-12-17 16:12:11 -0500

    Update script.

--- scripts/create_plugin_file.ts
+++ scripts/create_plugin_file.ts
@@ -1,12 +1,15 @@
 import * as path from "https://deno.land/std@0.130.0/path/mod.ts";
-import { extractCargoVersion, processPlugin } from "https://raw.githubusercontent.com/dprint/automation/0.3.0/mod.ts";
+import {
+  extractCargoVersionOrThrow,
+  processPlugin,
+} from "https://raw.githubusercontent.com/dprint/automation/0.6.0/mod.ts";
 
 const currentDirPath = path.dirname(path.fromFileUrl(import.meta.url));
 const cargoFilePath = path.join(currentDirPath, "../", "Cargo.toml");
 
 await processPlugin.createDprintOrgProcessPlugin({
   pluginName: "dprint-plugin-prettier",
-  version: await extractCargoVersion(cargoFilePath),
+  version: await extractCargoVersionOrThrow(cargoFilePath),
   platforms: [
     "darwin-x86_64",
     "linux-x86_64",
