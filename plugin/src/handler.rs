a3ecd434668fdecbf8dd2a9f8bdcdee533b2dfc0
David Sherret <dsherret@gmail.com>
2023-07-30 13:33:14 -0400

    feat: upgrade to dprint-core 0.63

--- plugin/src/handler.rs
+++ plugin/src/handler.rs
@@ -1,15 +1,16 @@
 use std::sync::Arc;
 
-use deno_core::futures::future;
+use dprint_core::async_runtime::async_trait;
+use dprint_core::async_runtime::LocalBoxFuture;
 use dprint_core::configuration::ConfigKeyMap;
 use dprint_core::configuration::GlobalConfiguration;
-use dprint_core::configuration::ResolveConfigurationResult;
 use dprint_core::plugins::AsyncPluginHandler;
-use dprint_core::plugins::BoxFuture;
+use dprint_core::plugins::FileMatchingInfo;
 use dprint_core::plugins::FormatRequest;
 use dprint_core::plugins::FormatResult;
-use dprint_core::plugins::Host;
+use dprint_core::plugins::HostFormatRequest;
 use dprint_core::plugins::PluginInfo;
+use dprint_core::plugins::PluginResolveConfigurationResult;
 use dprint_plugin_deno_base::channel::Channel;
 use dprint_plugin_deno_base::channel::CreateChannelOptions;
 use once_cell::sync::Lazy;
@@ -39,6 +40,7 @@ impl Default for PrettierPluginHandler {
   }
 }
 
+#[async_trait(?Send)]
 impl AsyncPluginHandler for PrettierPluginHandler {
   type Configuration = PrettierConfig;
 
@@ -47,8 +49,6 @@ impl AsyncPluginHandler for PrettierPluginHandler {
       name: env!("CARGO_PKG_NAME").to_string(),
       version: env!("CARGO_PKG_VERSION").to_string(),
       config_key: "prettier".to_string(),
-      file_extensions: SUPPORTED_EXTENSIONS.clone(),
-      file_names: vec![],
       help_url: "https://dprint.dev/plugins/prettier".to_string(),
       config_schema_url: "".to_string(),
       update_url: Some(
@@ -61,25 +61,32 @@ impl AsyncPluginHandler for PrettierPluginHandler {
     include_str!("../../LICENSE").to_string()
   }
 
-  fn resolve_config(
+  async fn resolve_config(
     &self,
     config: ConfigKeyMap,
     global_config: GlobalConfiguration,
-  ) -> ResolveConfigurationResult<Self::Configuration> {
-    resolve_config(config, global_config)
+  ) -> PluginResolveConfigurationResult<Self::Configuration> {
+    let result = resolve_config(config, global_config);
+    PluginResolveConfigurationResult {
+      config: result.config,
+      diagnostics: result.diagnostics,
+      file_matching: FileMatchingInfo {
+        file_extensions: SUPPORTED_EXTENSIONS.clone(),
+        file_names: vec![],
+      },
+    }
   }
 
-  fn format(
+  async fn format(
     &self,
     request: FormatRequest<Self::Configuration>,
-    _host: Arc<dyn Host>,
-  ) -> BoxFuture<FormatResult> {
+    _format_with_host: impl FnMut(HostFormatRequest) -> LocalBoxFuture<'static, FormatResult> + 'static,
+  ) -> FormatResult {
     if request.range.is_some() {
       // no support for range formatting
-      return Box::pin(future::ready(Ok(None)));
+      return Ok(None);
     }
 
-    let channel = self.channel.clone();
-    Box::pin(async move { channel.format(request).await })
+    self.channel.format(request).await
   }
 }
