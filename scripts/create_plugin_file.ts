7b3e31cf289f0e390a4532b47f454e5986dea176
David Sherret <dsherret@users.noreply.github.com>
2023-12-30 14:44:58 -0500

    chore: add linux-aarch64 to plugin file creation (#71)

--- scripts/create_plugin_file.ts
+++ scripts/create_plugin_file.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S deno run --allow-env --allow-read --allow-write --allow-net=deno.land
 import $ from "dax";
-import { extractCargoVersion, processPlugin } from "https://raw.githubusercontent.com/dprint/automation/0.3.0/mod.ts";
+import { CargoToml, processPlugin } from "https://raw.githubusercontent.com/dprint/automation/0.8.1/mod.ts";
 
 const currentDir = $.path(import.meta).parentOrThrow();
 const rootDir = currentDir.join("../").resolve();
@@ -8,10 +8,11 @@ const cargoFilePath = rootDir.join("plugin/Cargo.toml");
 
 await processPlugin.createDprintOrgProcessPlugin({
   pluginName: "dprint-plugin-prettier",
-  version: await extractCargoVersion(cargoFilePath.toString()),
+  version: new CargoToml(cargoFilePath).version(),
   platforms: [
     "darwin-x86_64",
     "linux-x86_64",
+    "linux-aarch64",
     "windows-x86_64",
   ],
   isTest: Deno.args.some(a => a == "--test"),
