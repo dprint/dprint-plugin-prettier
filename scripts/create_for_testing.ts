99dca6fce8b5c3094b052f5158aec80d76c318c6
David Sherret <dsherret@gmail.com>
2024-01-07 13:03:07 -0500

    Fix issue

--- scripts/create_for_testing.ts
+++ scripts/create_for_testing.ts
@@ -1,20 +1,21 @@
-#!/usr/bin/env -S deno run --allow-env --allow-read --allow-write --allow-net=deno.land
+#!/usr/bin/env -S deno run -A
 
 // Script for quickly creating the plugin for testing purposes on Windows
 // To run:
-// 1. Change the one line to `reference": `./${zipFileName}`,` in scripts/createPluginFile.js
-// 2. Run `./scripts/create_plugin_file.ts`
-// 3. Update dprint.json to point at ./plugin.json then update checksum
+// 1. Run `./scripts/create_plugin_file.ts`
+// 2. Update dprint.json to point at ./target/release/plugin.json then update checksum
 //    as shown when initially run.
 
 import $ from "dax";
 
-await $`deno task build`
-await $`cargo build --release`
+await $`deno task build`;
+await $`cargo build --release`;
 if (Deno.build.os === "windows") {
-  await $`powershell -Command ${"Compress-Archive -Force -Path target/release/dprint-plugin-prettier.exe -DestinationPath target/release/dprint-plugin-prettier-x86_64-pc-windows-msvc.zip"}`
+  await $`powershell -Command ${"Compress-Archive -Force -Path target/release/dprint-plugin-prettier.exe -DestinationPath target/release/dprint-plugin-prettier-x86_64-pc-windows-msvc.zip"}`;
+} else if (Deno.build.os === "linux") {
+  await $`zip -j target/release/dprint-plugin-prettier-x86_64-unknown-linux-gnu.zip target/release/dprint-plugin-prettier`;
 } else {
-  await $`zip -j target/release/dprint-plugin-prettier-linux.zip target/release/dprint-plugin-prettier`;
+  throw "TODO";
 }
 
 await $`cd target/release && deno run -A ../../scripts/create_plugin_file.ts --test`;
