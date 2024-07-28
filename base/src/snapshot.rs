12b46f06f5009844c5aa3e7a5f22898962b24f68
David Sherret <dsherret@gmail.com>
2024-07-28 15:04:14 -0400

    refactor: deno_core 0.299

--- base/src/snapshot.rs
+++ base/src/snapshot.rs
@@ -1,32 +1,14 @@
-use std::path::Path;
-
-pub struct Snapshot(Box<[u8]>);
-
-impl Snapshot {
-  pub fn from_path(file_path: &Path) -> std::io::Result<Self> {
-    Self::deserialize(&std::fs::read(file_path)?)
-  }
-
-  pub fn deserialize(data: &[u8]) -> std::io::Result<Self> {
-    // compressing takes about a minute in debug, so only do it in release
-    if cfg!(debug_assertions) {
-      Ok(Self(data.to_vec().into_boxed_slice()))
-    } else {
-      Ok(Self(
-        zstd::bulk::decompress(
-          &data[4..],
-          u32::from_le_bytes(data[0..4].try_into().unwrap()) as usize,
-        )?
-        .into_boxed_slice(),
-      ))
-    }
-  }
-
-  pub(crate) fn inner(&self) -> &[u8] {
-    &self.0
-  }
-
-  pub(crate) fn into_inner(self) -> Box<[u8]> {
-    self.0
+pub fn deserialize_snapshot(data: &[u8]) -> std::io::Result<Box<[u8]>> {
+  // compressing takes about a minute in debug, so only do it in release
+  if cfg!(debug_assertions) {
+    Ok(data.to_vec().into_boxed_slice())
+  } else {
+    Ok(
+      zstd::bulk::decompress(
+        &data[4..],
+        u32::from_le_bytes(data[0..4].try_into().unwrap()) as usize,
+      )?
+      .into_boxed_slice(),
+    )
   }
 }
--- plugin/build.rs
+++ plugin/build.rs
@@ -9,8 +9,6 @@ use std::process::Command;
 use deno_core::Extension;
 use dprint_plugin_deno_base::runtime::CreateRuntimeOptions;
 use dprint_plugin_deno_base::runtime::JsRuntime;
-use dprint_plugin_deno_base::runtime::StartupSnapshot;
-use dprint_plugin_deno_base::snapshot::Snapshot;
 use dprint_plugin_deno_base::util::create_tokio_runtime;
 
 fn main() {
@@ -19,7 +17,6 @@ fn main() {
   let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
   let startup_snapshot_path = out_dir.join("STARTUP_SNAPSHOT.bin");
   let js_dir = root_dir.join("js");
-  let js_src_dir = js_dir.join("node").join("src");
   let supported_extensions_path = out_dir.join("SUPPORTED_EXTENSIONS.json");
 
   let build_result = Command::new("deno").args(["task", "build"]).status();
@@ -38,43 +35,53 @@ fn main() {
   // ensure the build is invalidated if any of these files change
   println!(
     "cargo:rerun-if-changed={}",
-    js_src_dir.join("main.ts").display()
+    js_dir.join("node/main.ts").display()
   );
   println!(
     "cargo:rerun-if-changed={}",
-    js_src_dir.join("package.json").display()
+    js_dir.join("node/package.json").display()
   );
   println!(
     "cargo:rerun-if-changed={}",
-    js_src_dir.join("package-lock.json").display()
+    js_dir.join("node/package-lock.json").display()
   );
   println!(
     "cargo:rerun-if-changed={}",
-    js_src_dir.join("shims/url.js").display()
+    js_dir.join("node/shims/url.js").display()
   );
 
   let startup_code_path = js_dir.join("node/dist/main.js");
   if !startup_code_path.exists() {
     panic!("Run `deno task build` first.");
   }
-  create_snapshot(startup_snapshot_path.clone(), &startup_code_path);
+  let snapshot = create_snapshot(startup_snapshot_path.clone(), &startup_code_path);
+  let snapshot = Box::leak(snapshot);
 
   // serialize the supported extensions
   eprintln!("Creating runtime...");
   let tokio_runtime = create_tokio_runtime();
-  let snapshot = Snapshot::from_path(&startup_snapshot_path).unwrap();
+  JsRuntime::initialize_main_thread();
   let mut runtime = JsRuntime::new(CreateRuntimeOptions {
-    extensions: Vec::new(),
-    startup_snapshot: Some(StartupSnapshot::Boxed(snapshot)),
+    extensions: vec![
+      deno_webidl::deno_webidl::init_ops(),
+      deno_console::deno_console::init_ops(),
+      deno_url::deno_url::init_ops(),
+      main::init_ops(),
+    ],
+    startup_snapshot: Some(snapshot),
   });
 
   eprintln!("Getting extensions...");
-  let file_extensions = tokio_runtime
-    .block_on(runtime.execute_async_fn::<Vec<String>>(
-      "deno:get_extensions.js",
-      "dprint.getExtensions".to_string(),
-    ))
-    .unwrap();
+  let file_extensions: Vec<String> = tokio_runtime.block_on(async move {
+    let startup_text = get_startup_text(&startup_code_path);
+    runtime
+      .execute_script("dprint:prettier.js", startup_text.clone())
+      .unwrap();
+    runtime
+      .execute_async_fn::<Vec<String>>("deno:get_extensions.js", "dprint.getExtensions".to_string())
+      .await
+      .unwrap()
+  });
   std::fs::write(
     supported_extensions_path,
     deno_core::serde_json::to_string(&file_extensions).unwrap(),
@@ -83,8 +90,8 @@ fn main() {
   eprintln!("Done");
 }
 
-fn create_snapshot(snapshot_path: PathBuf, startup_code_path: &Path) {
-  let startup_text = std::fs::read_to_string(startup_code_path).unwrap();
+fn create_snapshot(snapshot_path: PathBuf, startup_code_path: &Path) -> Box<[u8]> {
+  let startup_text = get_startup_text(startup_code_path);
   dprint_plugin_deno_base::build::create_snapshot(
     dprint_plugin_deno_base::build::CreateSnapshotOptions {
       cargo_manifest_dir: env!("CARGO_MANIFEST_DIR"),
@@ -92,11 +99,16 @@ fn create_snapshot(snapshot_path: PathBuf, startup_code_path: &Path) {
       extensions: extensions(),
       with_runtime_cb: Some(Box::new(move |runtime| {
         runtime
-          .execute_script("dprint:prettier.js", startup_text.clone().into())
+          .execute_script("dprint:prettier.js", startup_text.clone())
           .unwrap();
       })),
+      warmup_script: None,
     },
-  );
+  )
+}
+
+fn get_startup_text(startup_code_path: &Path) -> String {
+  std::fs::read_to_string(startup_code_path).unwrap()
 }
 
 deno_core::extension!(
